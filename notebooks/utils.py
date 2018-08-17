b3d3ba54e3b0d6e6c94fcaaa5fb359de8acb6588
Zabamund <fracgeol@gmail.com>
2018-08-17 14:36:40 +0200

    moved function to utils

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -35,3 +35,53 @@ def mdia_to_xyz_minCurve(deviation):
     TVD_z = delta_MD / 2 * (np.cos(np.radians(incl_upper)) + np.cos(np.radians(incl_lower))) * RF_lower
     
     return east_x, north_y, TVD_z
+
+
+def top_incAziXYZ(tops_df, survey_edt):
+    """
+    Takes a tops dataframe and returns same dataframe with addtional columns
+    for Inc and Azi based on:
+    inc = inc_dev_lower - ((MD_dev_lower - MD_Top) * (delta_incl) / (delta_MD_dev))
+    azi = azi_dev_lower - ((MD_dev_lower - MD_Top) * (delta_azi) / (delta_MD_dev))
+    """
+    for top in tops_df.itertuples():
+        current_Top_MD = top[1] 
+        for depth in survey_edt.itertuples():
+            current_dev_MD = depth[1]
+            if current_Top_MD < current_dev_MD:
+                # get row above and below top
+                enclosing_rows = survey_edt[depth[0] - 1:depth[0] + 1]
+                # grab MD, incl, azi values from rows
+                MD_dev_low = enclosing_rows[1:2]['MD[m]']
+                MD_dev_up = enclosing_rows[:1]['MD[m]']
+                Inc_dev_low = enclosing_rows[1:2]['Inc[deg]']
+                Inc_dev_up = enclosing_rows[:1]['Inc[deg]']
+                Azi_dev_low = enclosing_rows[1:2]['Azi[deg]']
+                Azi_dev_up = enclosing_rows[:1]['Azi[deg]']
+                # grab x,y,z from rows
+                x_dev_low = enclosing_rows[1:2]['x[m]']
+                x_dev_up = enclosing_rows[:1]['x[m]']
+                y_dev_low = enclosing_rows[1:2]['y[m]']
+                y_dev_up = enclosing_rows[:1]['y[m]']
+                z_dev_low = enclosing_rows[1:2]['z[m]']
+                z_dev_up = enclosing_rows[:1]['z[m]']
+                # assign calculated value to correct column of correct row
+                # use .loc to avoid warning about assigning to a copy
+                tops_df.loc[top[:1][0],'Inc[deg]'] = Inc_dev_low.values[0] - ((MD_dev_low.values[0] - current_Top_MD) 
+                                                                * (Inc_dev_low.values[0] - Inc_dev_up.values[0]) 
+                                                                / (MD_dev_low.values[0] - MD_dev_up.values[0]))
+                tops_df.loc[top[:1][0],'Azi[deg]'] = Azi_dev_low.values[0] - ((MD_dev_low.values[0] - current_Top_MD) 
+                                                                * (Azi_dev_low.values[0] - Azi_dev_up.values[0]) 
+                                                                / (MD_dev_low.values[0] - MD_dev_up.values[0]))
+                
+                tops_df.loc[top[:1][0],'x[m]'] = x_dev_low.values[0] - ((MD_dev_low.values[0] - current_Top_MD) 
+                                                                * (x_dev_low.values[0] - x_dev_up.values[0]) 
+                                                                / (MD_dev_low.values[0] - MD_dev_up.values[0]))
+                tops_df.loc[top[:1][0],'y[m]'] = y_dev_low.values[0] - ((MD_dev_low.values[0] - current_Top_MD) 
+                                                                * (y_dev_low.values[0] - y_dev_up.values[0]) 
+                                                                / (MD_dev_low.values[0] - MD_dev_up.values[0]))
+                tops_df.loc[top[:1][0],'z[m]'] = z_dev_low.values[0] - ((MD_dev_low.values[0] - current_Top_MD) 
+                                                                * (z_dev_low.values[0] - z_dev_up.values[0]) 
+                                                                / (MD_dev_low.values[0] - MD_dev_up.values[0]))
+                break
+    return tops_df
